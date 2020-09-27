5ee63b5f7127a00d72fd2e2cf84cf3de7ff85674
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2020-09-27 15:17:21 +0200

    add findseed

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,6 +12,15 @@ class Fun(commands.Cog):
 	async def rng(self, ctx, minimum: int, maximum: int):
 		await ctx.send(randint(minimum, maximum))
 
+	@commands.command(description='Simulates Minecraft end portal generation')
+	async def findseed(self, ctx):
+		eyes = 0
+		for i in range(12):
+			if randint(0, 99) < 10:
+				eyes += 1
+
+		await ctx.send(f'{ctx.author.mention} -> your seed is a {eyes} eye')
+
 
 def setup(bot):
 	bot.add_cog(Fun(bot))
